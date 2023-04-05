e3bb7be123dbb42729d81e0ae5edd5db4a879e5f
cafferatielisa <cafferati.elisa@iisgalvanimi.edu.it>
2023-04-05 10:57:24 +0000

    clash royale

--- templates_flask/app.py
+++ templates_flask/app.py
@@ -1,6 +1,15 @@
 from flask import render_template
 from flask import Flask
+import mysql.connector
 
+#Connect to mysql
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="CLASH_ROYALE"
+)
+mycursor = mydb.cursor()
 
 app = Flask(__name__)
 
@@ -10,8 +19,8 @@ def hello():
     return render_template('hello.html', name='Elisa')
 
 
-    @app.route('/units')
+@app.route('/units')
 def unitList():
         mycursor.execute("SELECT * FROM Clash_Unit")
-        my result = mycursur.fetchall()
+        myresult = mycursor.fetchall()
         return render_template('clash_units.html', units=myresult)
